217fbec3d17dafea2d2aaca7a92c9b17904bbb9d
vincent ~ <vno16@cs19110bs.canterbury.ac.nz>
2022-09-30 16:32:01 +1300

    feat: Module header for Blue LED

---
+++ led.h
@@ -0,0 +1,43 @@
+//
+//  led.h
+//  
+//  Module for controlling the blue led because the existing driver one is confusing :)
+//
+//  Authored by vno16 and ski102 on 30 Sep 2022
+//
+
+#ifndef LED_H
+#define LED_H
+
+#include <stdbool.h>
+#include "pio.h"
+#include "system.h"
+
+/*!
+ * \brief Initialise the Blue LED PIO as output start as turned off
+ */
+void led_init(void);
+
+/*!
+ * \brief Turn on Blue LED
+ */
+void led_on(void);
+
+/*!
+ * \brief Turn off Blue LED
+ */
+void led_off(void);
+
+/*!
+ * \brief Toggle LED from on to off or off to on
+ */
+void led_toggle(void);
+
+/*!
+ * \brief Get the status of the LED
+ * 
+ * \returns True if the LED is currently on
+ */
+bool led_active_p(void);
+
+#endif
