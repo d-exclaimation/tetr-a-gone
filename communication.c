25d8964f9ced23ce43bfbf239fe73d52f446abc9
Vincent ~ <vno16@uclive.ac.nz>
2022-10-09 14:11:36 +1300

    fix: Fixed led turning on if valid message received

--- communication.c
+++ communication.c
@@ -25,12 +25,10 @@ void comms_subscribe(Hexagone_t* game)
     if (!ir_uart_read_ready_p()) {
         return;
     }
-
     
-    led_on();
-
     Packet_t packet = ir_uart_getc();
     Message_t msg = message_decode(packet);
+
     switch (msg.typeno) {
         
         // Update the other player
@@ -38,20 +36,28 @@ void comms_subscribe(Hexagone_t* game)
             game->other = msg.payload;            
             break;
 
+        // Perform physics on the platform
         case PLATFORM_FORCE:
             hexagone_physics(game, msg.payload);
             break;
 
+        // End the game if not ended already
         case GAME_OVER:
             game->state = hexagone_ended_p(game) ? game->state : WIN;
             break;
 
         default:
-            break;
+            return;
     }
 
-    if (hexagone_fallen_p(game, game->player))
+    // Turn on LED everytime a valid message is received
+    led_on();
+
+    // Send an ending message if the player fell or had fallen after receiving a message
+    if (hexagone_fallen_p(game, game->player)) {
         comms_publish(message_end());
+    }
     
+    // Performs checks
     hexagone_audit(game);
 }
